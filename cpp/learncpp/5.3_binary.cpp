0947ffdd287a9f0b3977a1bbc1334b201c48bf68
yangnanbei <406631132@qq.com>
2024-06-07 23:32:59 +0800

    Improvement, cpp. binary output,yang_nanbei

---
+++ cpp/learncpp/5.3_binary.cpp
@@ -0,0 +1,14 @@
+#include <bitset> // for std::bitset
+#include <iostream>
+
+int main()
+{
+	// std::bitset<8> means we want to store 8 bits
+	std::bitset<8> bin1{ 0b1100'0101 }; // binary literal for binary 1100 0101
+	std::bitset<8> bin2{ 0xC5 }; // hexadecimal literal for binary 1100 0101
+
+	std::cout << bin1 << '\n' << bin2 << '\n';
+	std::cout << std::bitset<4>{ 0b1010 } << '\n'; // create a temporary std::bitset and print it
+
+	return 0;
+}
