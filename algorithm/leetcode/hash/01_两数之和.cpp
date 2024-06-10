373a024c6ffcc02822e0ac464da99d5dcd04cce7
yangnanbei <406631132@qq.com>
2024-06-10 15:26:06 +0800

    Improvement, leetcode, now we can debug on vscode, yang_nanbei

--- algorithm/leetcode/hash/01_两数之和.cpp
+++ algorithm/leetcode/hash/01_两数之和.cpp
@@ -1,16 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
+        if (!nums.size()) {
+            return {};
+        }
+        std::unordered_map<int, int>  umap;
+        int target_num;
         for (int i = 0; i < nums.size(); i++) {
-            int tar_val = target - nums[i];
-            if(mp.count(tar_val)) {
-                return {mp[tar_val], i};
-            } else {
-                mp.insert(pair<int, int>(nums[i], i));
+            target_num = target - nums[i];
+            if (umap.count(target_num)) {
+                return {umap[target_num], i};
             }
+            umap.insert(pair<int, int>(nums[i], i));
         }
         return {};
     }
 };
+
+int main() {
+    Solution solution;
+    // 输入
+    vector<int> nums = {2, 7, 11, 15, 3, 5};
+    int target = 8;
+
+    // 输出
+    vector<int> result = solution.twoSum(nums, target);
+
+    // 打印结果
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+}
