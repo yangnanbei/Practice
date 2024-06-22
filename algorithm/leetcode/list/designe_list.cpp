5aa7868a17158cffdb4a5ffc25b60bd3b06e3a14
yangnanbei <406631132@qq.com>
2024-06-22 11:04:32 +0800

    Improvement, leetcode, design list, yang_nanbei

--- algorithm/leetcode/list/designe_list.cpp
+++ algorithm/leetcode/list/designe_list.cpp
@@ -9,85 +9,113 @@
 #include <iostream>
 using namespace std;
 
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode(): val(0), next(nullptr) {}
-	ListNode(int x): val(x), next(nullptr) {}
-	ListNode(int x, ListNode *p): val(x), next(p) {}
-}
-
 class MyLinkedList {
 public:
-    MyLinkedList() {
-		sentinel = new ListNode(0);
-		size = 0;
-    }
-    
-    int get(int index) {
-		ListNode *tmp;
-		if (index > 0 || index > (size_ - 1)) {
-			cout << "get: invalid index " << index << endl;
-		}
+    // 定义链表节点结构体
+    struct LinkedNode {
+        int val;
+        LinkedNode* next;
+        LinkedNode(int val):val(val), next(nullptr){}
+    };
 
-		tmp = sentibel->nex;
-		while (index--) {
-			tmp = tmp->next;
-		}
-		return tmp->val;
+    // 初始化链表
+    MyLinkedList() {
+        _dummyHead = new LinkedNode(0); // 这里定义的头结点 是一个虚拟头结点，而不是真正的链表头结点
+        _size = 0;
     }
-    
+
+    // 获取到第index个节点数值，如果index是非法数值直接返回-1， 注意index是从0开始的，第0个节点就是头结点
+    int get(int index) {
+        if (index > (_size - 1) || index < 0) {
+            return -1;
+        }
+        LinkedNode* cur = _dummyHead->next;
+        while(index--){ // 如果--index 就会陷入死循环
+            cur = cur->next;
+        }
+        return cur->val;
+    }
+
+    // 在链表最前面插入一个节点，插入完成后，新插入的节点为链表的新的头结点
     void addAtHead(int val) {
-		ListNode* insnode = new ListNode(val);
-		insnode->next = sentinel->next; 
-		sentinel->next = insnode;
+        LinkedNode* newNode = new LinkedNode(val);
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
     }
-    
+
+    // 在链表最后面添加一个节点
     void addAtTail(int val) {
-		ListNode *insnode = new ListNode(val);
-		ListNode *tmp = sentinel->next;
-		while(tmp--) {
-			tmp = tmp->next;
-		}
-		tmp->next = insnode;
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(cur->next != nullptr){
+            cur = cur->next;
+        }
+        cur->next = newNode;
+        _size++;
     }
-    
+
+    // 在第index个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+    // 如果index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+    // 如果index大于链表的长度，则返回空
+    // 如果index小于0，则在头部插入节点
     void addAtIndex(int index, int val) {
-		if (index < 0 || index > (size_ - 1)) {
-			cout << "addAtIndex: invalid index " << index << endl;
-		}
-		ListNode* insnode = new ListNode(val);
-		ListNode *tmp = sentinel->next;
-		while(index--) {
-			tmp = tmp->next;
-		}
-		insnode->next = tmp->next;
-		tmp->next = insnode;
+
+        if(index > _size) return;
+        if(index < 0) index = 0;        
+        LinkedNode* newNode = new LinkedNode(val);
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
     }
-    
+
+    // 删除第index个节点，如果index 大于等于链表的长度，直接return，注意index是从0开始的
     void deleteAtIndex(int index) {
-		if (index < 0 || index > (size_ - 1)) {
-			cout << " deleteAtIndex: invalid index " << index << endl;
-		}
-		ListNode *tmp = sentinel->next;
-		while(index--) {
-			tmp = tmp->next;
-		}
-		ListNode *delnode = tmp;
-		tmp->next = tmp->next->next;
-		delete tmp;
+        if (index >= _size || index < 0) {
+            return;
+        }
+        LinkedNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur ->next;
+        }
+        LinkedNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+        //delete命令指示释放了tmp指针原本所指的那部分内存，
+        //被delete后的指针tmp的值（地址）并非就是NULL，而是随机值。也就是被delete后，
+        //如果不再加上一句tmp=nullptr,tmp会成为乱指的野指针
+        //如果之后的程序不小心使用了tmp，会指向难以预想的内存空间
+        tmp=nullptr;
+        _size--;
+    }
+
+    // 打印链表
+    void printLinkedList() {
+        LinkedNode* cur = _dummyHead;
+        while (cur->next != nullptr) {
+            cout << cur->next->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
     }
 private:
-	int size_;
-	ListNode* sentinel;
+    int _size;
+    LinkedNode* _dummyHead;
+
 };
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList* obj = new MyLinkedList();
- * int param_1 = obj->get(index);
- * obj->addAtHead(val);
- * obj->addAtTail(val);
- * obj->addAtIndex(index,val);
- * obj->deleteAtIndex(index);
- */
+main()
+{
+	MyLinkedList mll;
+	mll.addAtHead(1);
+	mll.addAtTail(3);
+	mll.addAtIndex(1,2);
+	cout << mll.get(1) << endl;
+	mll.deleteAtIndex(1);
+	cout << mll.get(1) << endl;
+	return 0;
+}
