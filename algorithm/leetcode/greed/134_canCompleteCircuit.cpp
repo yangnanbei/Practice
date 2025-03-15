1ebd39bbab4a52493970bb00cf467233e385c140
yangnanbei <406631132@qq.com>
2025-03-15 19:58:53 +0800

    leetcode, greed, 134 can complete circuit, yang_nanbei

---
+++ algorithm/leetcode/greed/134_canCompleteCircuit.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = 0;
+        int cur_sum = 0;
+        int total_sum = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            cur_sum += gas[i] - cost[i];
+            total_sum += gas[i] - cost[i]; 
+            if (cur_sum < 0) {
+                start = i + 1;
+                cur_sum = 0;
+            }
+
+        }
+
+        if (total_sum < 0) {
+            return -1;
+        }
+        
+        return start;
+    }
+};
