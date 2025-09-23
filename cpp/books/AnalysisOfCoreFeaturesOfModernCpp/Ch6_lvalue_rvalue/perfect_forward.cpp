2cf5c044199ae471553da694fcb231f5dcb99acf
yangnanbei <406631132@qq.com>
2025-09-23 23:26:31 +0800

    perfect forward, yang_nanbei

--- cpp/books/AnalysisOfCoreFeaturesOfModernCpp/Ch6_lvalue_rvalue/perfect_forward.cpp
+++ cpp/books/AnalysisOfCoreFeaturesOfModernCpp/Ch6_lvalue_rvalue/perfect_forward.cpp
@@ -13,8 +13,33 @@ void normal_forwarding(T t)
     show_type(t);
 }
 
+template<class T>
+void ref_forwarding(T &t)
+{
+    show_type(t);
+}
+
+template<class T>
+void const_ref_forwarding(const T &t)
+{
+    show_type(t);
+}
+
+template<class T>
+void perfect_forwarding(T &&t)
+{
+    show_type(std::forward<T>(t));
+}
+
+std::string get_string()
+{
+    return "hello";
+}
+
 int main()
 {
     std::string s = "hello world";
     normal_forwarding(s);
+    perfect_forwarding(s);
+    //normal_forwarding(get_string());
 }
